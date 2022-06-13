036919d3515e3608ef34ccb3aa2c5348f16c76d2
stensis <irenenjuguna98@gmail.com>
2022-06-13 21:19:12 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+// write your code here!
+
+document.addEventListener('DOMContentLoaded', function(){
+document.querySelector('#text').textContent = "This is really cool!"
+})
